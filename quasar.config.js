ca14ef77a3c3559ef9aec8e698e45710c1f78d5e
pallascodes <pallascodes@gmail.com>
2023-10-18 12:40:35 -0600

    env variables setup changed

--- quasar.config.js
+++ quasar.config.js
@@ -11,7 +11,7 @@
 const { configure } = require('quasar/wrappers')
 const path = require('path')
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx) {
   return {
     eslint: {
       // fix: true,
@@ -63,7 +63,11 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      env: require('dotenv').config().parsed,
+      env: {
+        SERVER_URL: ctx.dev
+          ? 'http://localhost:3000/api'
+          : 'https://inventory-n52k.onrender.com/api',
+      },
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
