1b61ddcdf72c717674f3680f737569e2c5fe8e53
pallascodes <pallascodes@gmail.com>
2023-10-19 17:51:08 -0600

    page title and document title change on route implemented

---
+++ src/stores/routes-store.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+export const useRouteStore = defineStore('store', () => {
+  const pageTitle = ref<string>('')
+
+  return {
+    // State
+    pageTitle,
+
+    // Computed
+    getPageTitle: computed(() => pageTitle.value),
+
+    // Actions
+    setPageTitle(title: string) {
+      document.title = `Inventario IM - ${title}`
+      pageTitle.value = title
+    },
+  }
+})
